36cc87f3b17fd6307dcad7afcd51c4f96eb8072d
Deeksha Dangwal <deekshadangwal@users.noreply.github.com>
2016-08-15 14:22:45 -0500

    access to range of servers from servers.txt

--- srunssh.sh
+++ srunssh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# if you want to access only a range of servers (say from line 3 to 15 of servers.txt):
+# for f in `cat servers.txt | sed '3,15!d'` ; 
 for f in `cat servers.txt`;
 do
   echo "This is $f";
